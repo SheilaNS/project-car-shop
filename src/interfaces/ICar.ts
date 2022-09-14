259b1cc0b1ce229856a21cc279ed47f7cc695274
SheilaNS <shei.nsantos@gmail.com>
2022-09-14 16:56:32 -0300

    feat(Requisito 03): :sparkles: Requisito finalizado
    Interface ICar criada

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+import { VehicleZodSchema } from './IVehicle';
+
+const CarZodSchema = z.object({
+  doorsQty: z.number().int().positive().gte(2)
+    .lte(4),
+  seatsQty: z.number().int().gte(2).lte(7),
+});
+
+const Car = CarZodSchema.merge(VehicleZodSchema);
+type ICar = z.infer<typeof Car>;
+
+export { CarZodSchema, ICar };
